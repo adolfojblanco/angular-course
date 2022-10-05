fb6bc57bcf02d71844e9a58abccb95229456ef2c
Adolfo Blanco <ajb156@outlook.com>
2022-10-05 12:53:00 +0200

    Updat

---
+++ formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [],
+})
+export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    console.log(this.miFormulario);
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched
+  }
+}
