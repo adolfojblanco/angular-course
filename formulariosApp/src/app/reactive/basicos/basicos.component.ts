7993038b57f432c5e0fd938f03e76837cec9c6f3
Adolfo Blanco <ajb156@outlook.com>
2022-10-07 19:49:40 +0200

    Update form

--- formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent implements OnInit {
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(0),
+  // });
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencia: [, [Validators.required, Validators.min(0)]],
+  });
 
-  ngOnInit(): void {
+  // FormBuilder es un servicio por eso lo inyectamos
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  // Para las validaciones del formulario.
+  // Enviamos el nombre en la funcion, asi no lo repetimos
+  campoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
   }
 
+  guardar() {
+    //console.log(this.miFormulario.value)
+
+    // Si el forrmulario es invalido, no continuamos
+    if (this.miFormulario.invalid) {
+      // Con esto marcas, los campos como si fueran tocados
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    this.miFormulario.reset();
+  }
 }
--- formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,90 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Death Strangin', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
+
+  // esto es para agregar un nuevo favorito
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  /**
+   * Obtener arreglo del formulario favorito
+   */
+  get favoritosArr() {
+    // Con esto le decimos a TS que es un FormArray, para que no nos de errores en el html.
+    return this.miFormulario.controls['favoritos'] as FormArray;
   }
 
+  /**
+   * Agrega un nuevo favorito
+   */
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    this.favoritosArr.push(
+      new FormControl(this.nuevoFavorito.value, Validators.required)
+    );
+    this.nuevoFavorito.reset();
+  }
+
+  /**
+   * Eliminar un favorito del arreglo
+   */
+  eliminarFavorito(i: number) {
+    console.log(i);
+    // Remueve un objeto de la posicion que le pasemos el indice.
+    this.favoritosArr.removeAt(i);
+  }
+
+  /**
+   * Recibe el nombre del campo del formulario para validarlo
+   * @param campo nombre del campo.
+   * @returns true or false
+   */
+  campoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  /**
+   * Guarda un formulario
+   */
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+
+    // Reinicia el formulario
+    //this.miFormulario.reset();
+  }
 }
