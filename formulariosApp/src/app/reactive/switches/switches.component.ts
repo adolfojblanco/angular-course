a53858c9e501602acffe7d7ed263431bb63d943d
Adolfo Blanco <ajb156@outlook.com>
2022-10-12 12:54:09 +0200

    Update

--- formulariosApp/src/app/reactive/switches/switches.component.ts
+++ formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  /**
+   * Nuevo formulario
+   */
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: ['M', Validators.required],
+    terminos: [false, Validators.required],
+  });
 
-  constructor() { }
+  /**
+   * Objeto persona
+   */
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    // Usamos reset en vez de set para que no nos de un error al establecer los valores
+    this.miFormulario.reset({
+      ...this.persona,
+      terminos: false,
+    });
+
+    // Esto para los cambios sincronos
+    this.miFormulario.valueChanges.subscribe((form) => {
+      console.log(form);
+    });
   }
 
+  guardar() {
+    const form = { ...this.miFormulario.value };
+    console.log(form);
+  }
 }
